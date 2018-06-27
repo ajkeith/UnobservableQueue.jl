7693aa13c760bcde93a3b0d0641d0b8df338901a
ajkeith <keith.andrew.j@gmail.com>
2018-06-27 10:34:22 -0400

    added disorder and deltamax

--- src/UnobservableQueue.jl
+++ src/UnobservableQueue.jl
@@ -55,3 +55,75 @@ function runsim(s::sim, q::queue)
     departorder = convert(Vector{Int64}, sortrows(depart, by = x -> (x[2],x[1],x[3]))[:,3])
     DataFrame(aorder = arriveorder, atime = arrivetime, stime = starttime, dorder = departorder, dtime = departtime)
 end
+
+# Disorder Function
+# Create noisy data for robust methods
+# Input: queue sim output
+# Output: true and noisy arrival and departure data
+function disorder(r_out::Array{Float64,2}, num_servers::Int64)
+  # actual data from simulation output
+  A = r_out[:, 1] # arrive_order
+  D_num = r_out[:, 3] # depart_order
+  D_sort = r_out[:, 4] # depart_time
+  D_raw = sort(D_sort) # sorted depart_time
+  # D_raw_num = sortrows(r_out[:,3:4], by = x -> x[1])
+
+  # constants and data structures
+  NUM_SERVERS = num_servers
+  MAX_CUSTS = length(r_out[:, 1])
+  results = DataFrame()
+
+  # Introduce partial observation error on cust depart order and time
+  D_num_err = zeros(MAX_CUSTS)
+  D_num_err[1:MAX_CUSTS] = D_num[1:MAX_CUSTS] # order error
+  D_err = zeros(MAX_CUSTS) # misordered depart times
+  D_err[1:MAX_CUSTS] = D_sort[1:MAX_CUSTS] # misordered depart times
+  for i = 1:(MAX_CUSTS-NUM_SERVERS - 1)
+    if rand() < 0.1
+      swap = i + rand(1:NUM_SERVERS)
+      temp1 = D_num_err[i]
+      temp2 = D_err[i]
+      D_num_err[i] = D_num_err[swap]
+      D_num_err[swap] = temp1
+      D_err[i] = D_err[swap]
+      D_err[swap] = temp2
+    end
+  end
+  len = size(D_raw, 1)
+  D_raw_meas = zeros(len) # measure error in depart times (original order)
+  D_raw_meas[1] = D_raw[1] / 2 + rand() * mean(D_raw[1:2]) - D_raw[1] / 2
+  D_raw_meas[len] = D_raw[len]
+  for i = 2:(len - 1)
+    a = mean(D_raw[(i-1):i])
+    b = mean(D_raw[i:(i+1)])
+    D_raw_meas[i] = a + rand() * (b - a)
+  end
+  D_meas = zeros(len)
+  incr = 0
+  for ind in convert.(Int64, r_out[:,3])
+      incr += 1
+      D_meas[incr] = D_raw_meas[ind]
+  end
+
+  DataFrame(x = 1:MAX_CUSTS, Arrive = A[1:MAX_CUSTS],
+    Depart = D_sort[1:MAX_CUSTS], DepartMeas = D_meas[1:MAX_CUSTS],
+    DepartErr = D_err[1:MAX_CUSTS], DepartOrder = D_num[1:MAX_CUSTS],
+    DepartOrderErr = D_num_err[1:MAX_CUSTS])
+end
+
+
+# Deltamax Estimator
+# Estimate the number of servers using the Deltamax algorithm
+# Input: departure order
+# Output: estimated number of servers
+function c_deltamax(out_order::Vector{Number})
+  N = size(out_order, 1)
+  currmax = out_order[1]
+  deltamax = 0
+  for i = 2:N
+    nextmax = max(out_order[i], currmax)
+    deltamax = max(deltamax, nextmax - currmax)
+    currmax = nextmax
+  end
+  deltamax
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,39 +2,71 @@ include("C:\\Users\\op\\Documents\\Julia Projects\\UnobservableQueue.jl\\src\\Un
 using Base.Test
 using Distributions
 
-@testset "Queue Simulation" begin
-    # M/M/2 queue
-    c = 2 # number of servers
-    μ = 1 / c # service rate (mean service time is 1/μ) (single server)
-    λ = 0.9 # arrival rate (mean interarrival time is 1/λ)
-    adist = Exponential(1/λ) # arrival distribution
-    sdist = Exponential(1/μ) # service distribution (single server)
-    ncust = 10_000 # total number of customers generated
-    timelimit = 1_000 # time limit for simulation
-    seed = 8710 # rng seed
-    s1 = sim(ncust, timelimit, seed)
-    q1 = queue(c, adist, sdist)
-    df = runsim(s1, q1)
-    @test mean(df[:dtime] - df[:stime]) ≈ (1 / μ) atol = 0.1
-    @test df[:atime] |> diff |> mean ≈ (1 / λ) atol = 0.1
-    @test all(diff(df[:atime]) .> 0)
-    @test all(diff(sort(df, :dorder)[:dtime]) .> 0)
+@testset "Unobservable Queue" begin
+    @testset "Queue Simulation" begin
+        # M/M/2 queue
+        c = 2 # number of servers
+        μ = 1 / c # service rate (mean service time is 1/μ) (single server)
+        λ = 0.9 # arrival rate (mean interarrival time is 1/λ)
+        adist = Exponential(1/λ) # arrival distribution
+        sdist = Exponential(1/μ) # service distribution (single server)
+        ncust = 10_000 # total number of customers generated
+        timelimit = 1_000 # time limit for simulation
+        seed = 8710 # rng seed
+        s1 = sim(ncust, timelimit, seed)
+        q1 = queue(c, adist, sdist)
+        df1 = runsim(s1, q1)
+        @test mean(df1[:dtime] - df1[:stime]) ≈ (1 / μ) atol = 0.1
+        @test df1[:atime] |> diff |> mean ≈ (1 / λ) atol = 0.1
+        @test all(diff(df1[:atime]) .> 0)
+        @test all(diff(sort(df1, :dorder)[:dtime]) .> 0)
 
-    # G/G/15 queue
-    c = 15 # number of servers
-    μa = (1 / c) * 0.99
-    μs = 1 / c
-    σ = 0.9
-    adist = LogNormal(exp(1/μ), σ) # arrival distribution
-    sdist = LogNormal(exp(1/μ), σ) # service distribution (single server)
-    ncust = 10_000 # total number of customers generated
-    timelimit = 10_000 # time limit for simulation
-    seed = 3001 # rng seed
-    s2 = sim(ncust, timelimit, seed)
-    q2 = queue(c, adist, sdist)
-    df = runsim(s2, q2)
-    @test mean(df[:dtime] - df[:stime]) ≈ mean(rand(sdist,10_000)) atol = 100
-    @test df[:atime] |> diff |> mean ≈ mean(rand(adist, 10_000)) atol = 100
-    @test all(diff(df[:atime]) .> 0)
-    @test all(diff(sort(df, :dorder)[:dtime]) .> 0)
+        # G/G/15 queue
+        c = 15 # number of servers
+        μa = (1 / c) * 0.99
+        μs = 1 / c
+        σ = 0.9
+        adist = LogNormal(exp(1/μ), σ) # arrival distribution
+        sdist = LogNormal(exp(1/μ), σ) # service distribution (single server)
+        ncust = 10_000 # total number of customers generated
+        timelimit = 10_000 # time limit for simulation
+        seed = 3001 # rng seed
+        s2 = sim(ncust, timelimit, seed)
+        q2 = queue(c, adist, sdist)
+        df = runsim(s2, q2)
+        @test mean(df[:dtime] - df[:stime]) ≈ mean(rand(sdist, 10_000)) atol = 100
+        @test df[:atime] |> diff |> mean ≈ mean(rand(adist, 10_000)) atol = 100
+        @test all(diff(df[:atime]) .> 0)
+        @test all(diff(sort(df, :dorder)[:dtime]) .> 0)
+    end
+
+    @testset "Server Estimation" begin
+        # Introduce observation error into true data
+        c = 2 # number of servers
+        μ = 1 / c # service rate (mean service time is 1/μ) (single server)
+        λ = 0.9 # arrival rate (mean interarrival time is 1/λ)
+        adist = Exponential(1/λ) # arrival distribution
+        sdist = Exponential(1/μ) # service distribution (single server)
+        ncust = 10_000 # total number of customers generated
+        timelimit = 1_000 # time limit for simulation
+        seed = 8710 # rng seed
+        s1 = sim(ncust, timelimit, seed)
+        q1 = queue(c, adist, sdist)
+        df1 = runsim(s1, q1)
+        simout = hcat(df1[:atime], df1[:stime], df1[:dorder], df1[:dtime], df1[:dtime] - df1[:stime], df1[:stime] - df1[:atime], df1[:aorder])
+        noiseout = disorder(simout, 2)
+        @test count(noiseout[:DepartOrderErr] - noiseout[:DepartOrder] .== 0.0) / size(noiseout,1) ≈ 0.9 atol = 0.1
+        @test all(sort(noiseout, :DepartMeas)[:DepartOrder] .== collect(1:size(noiseout,1)))
+
+        # Deltamax algorithm
+        o1 = [1,2,3,5,4]
+        o2 = [1,3,6,5,4]
+        @test c_deltamax(o1) == 2 && c_deltamax(o2) == 3
+
+        # Variance algorithm
+
+        # Order-based algorithm
+
+        # Comparison
+    end
 end
